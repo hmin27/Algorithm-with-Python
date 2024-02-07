2789638aacd35b13e442886bee72580aa0fe56da
hmin27 <wga0523@naver.com>
2024-02-08 00:48:23 +0900

    Problem Solved

---
+++ 1932.py
@@ -0,0 +1,33 @@
+# 2024-02-08
+# 동적 계획법
+# [1932] 정수 삼각형
+
+import sys
+
+total = []
+arr = []
+
+# 마지막 행부터 최소값 저장
+def calculate_cost(row):
+    global total
+
+    for i in range(row-2, -1, -1):  # n-2~0행
+        temp = [ 0 for _ in range(i+1) ]
+        for j in range(i+1):
+            maximum = max(arr[i][j]+total[j], arr[i][j]+total[j+1])
+            temp[j] = maximum
+
+        total = temp
+
+
+if __name__ == "__main__":
+    n = int(input())
+
+    for i in range(n):
+        arr.append(list(map(int, sys.stdin.readline().split())))
+    
+    total = arr[n-1]
+    
+    calculate_cost(n)
+    print(total[0])
+    
