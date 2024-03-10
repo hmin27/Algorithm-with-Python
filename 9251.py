9ff12ae011990120bcd92640147935f6da0f1f65
hmin27 <wga0523@naver.com>
2024-03-10 22:00:56 +0900

    Problem Solved: Dynamic Programming

--- 9251.py
+++ 9251.py
@@ -14,28 +14,17 @@ if len(seq_a) > len(seq_b):
     max_seq = seq_a
 
 
-dp = [[ 0 for _ in range(len(max_seq)) ] for _ in range(len(min_seq)+1) ]  # (min_seq + 1) x max_seq
+dp = [[ 0 for _ in range(len(max_seq)+1) ] for _ in range(len(min_seq)+1) ]  # (min_seq + 1) x (max_seq + 1)
 
-# 작은 시퀀스를 기준으로 겹치는 부분 수열 파악
-# 작은 시퀀스를 0부터 끝까지 하나씩 가면서 겹치는 문자 있는지 확인
-# 겹치는 문자가 있을 때 해당 문자 왼쪽으로 max에 +1해서 저장
+for i, letter in enumerate(min_seq, start=1):
 
-for i, letter in enumerate(min_seq):
+    for j in range(1, len(max_seq)+1):
+        if letter == max_seq[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
 
-    for j in range(len(max_seq)):
-        if letter == max_seq[j]:
-            if j != 0:
-                temp = max(dp[i][:j])
-                dp[i+1][j] = max(temp+1, 1)
-            else: 
-                dp[i+1][j] = 1
         else: 
-            dp[i+1][j] = dp[i][j]
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-print(max(dp[-1]))
+# print(dp)
+print(dp[-1][-1])
             
-        
-
-
-
-
