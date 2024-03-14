ace536dba37804423ec0cbc30d289c7af98e059f
hmin27 <wga0523@naver.com>
2024-03-14 11:37:25 +0900

    Problem Solved: Prefix sum

--- 10986.py
+++ 10986.py
@@ -3,23 +3,25 @@
 # [10986] 나머지 합
 
 import sys
+import math
 input = sys.stdin.readline
 
 n, div = map(int, input().split())
 arr = list(map(int, input().split()))
 
-prefix_sum = [ 0 ]
+prefix_sum = [ 0 ]  # 0~n
+remain = [ 0 for _ in range(div) ] # 0~div-1
+remain[0] = 1
 count = 0
+total = 0
 
 for i in range(n):
-    prefix_sum.append( prefix_sum[i] + arr[i] )
+    add = prefix_sum[i] + arr[i]
+    prefix_sum.append(add)
+    remain[add % div] += 1
 
-for i in range(1, n):
-    for j in range(i, n+1):
-        result = prefix_sum[j] - prefix_sum[i-1]
 
-        if(result % div == 0):
-            count += 1
-        
+for i in range(div):
+    total += math.comb(remain[i], 2)
 
-print(count)
+print(total)
