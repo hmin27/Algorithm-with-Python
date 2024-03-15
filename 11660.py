24f98799d96ce80fa76ba5ca4f10754bea34cf9e
hmin27 <wga0523@naver.com>
2024-03-15 17:53:39 +0900

    Problem Solved: Prefix sum

--- 11660.py
+++ 11660.py
@@ -14,13 +14,12 @@ for i in range(n):
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        prefix_sum[i][j] = arr[i-1][j-1] + prefix_sum[i][j-1]
+        prefix_sum[i][j] = arr[i-1][j-1] + prefix_sum[i][j-1] + prefix_sum[i-1][j] - prefix_sum[i-1][j-1]
 
 for _ in range(repeat):
     result = 0
     x1, y1, x2, y2 = map(int, input().split())
     
-    for j in range(x1, x2+1):
-        result += (prefix_sum[j][y2] - prefix_sum[j][y1-1])
+    result = prefix_sum[x2][y2] - prefix_sum[x1-1][y2] - prefix_sum[x2][y1-1] + prefix_sum[x1-1][y1-1]
     
     print(result)
